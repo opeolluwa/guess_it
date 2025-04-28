68817c7f31dd650e2c2929ca20206e8f70e047f4
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-04-28 23:25:39 +0100

    lint

--- src/main.c
+++ src/main.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    const char *query = "CREATE TABLE IF NOT EXISTS player(identifier INTEGER PRIMARY KEY, user_name TEXT)";
+    const char *query = "CREATE TABLE IF NOT EXISTS player(identifier INTEGER PRIMARY KEY, user_name TEXT, score INTEGER DEFAULT 0)";
     if (sqlite3_exec(db, query, 0, 0, &err_msg) != SQLITE_OK)
     {
         fprintf(stderr, "Error creating table: %s\n", err_msg);
